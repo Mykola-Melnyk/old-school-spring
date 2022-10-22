ea71f0fabbdbbd9bd90ae4c252b437c452aee57c
Mykola Melnyk <mr.nicholas.x@gmail.com>
2022-10-22 18:26:09 +0300

    code reformat

--- src/main/java/com/zoolatech/loganalyzer/LoganalyzerApplication2.java
+++ src/main/java/com/zoolatech/loganalyzer/LoganalyzerApplication2.java
@@ -1,17 +1,15 @@
 package com.zoolatech.loganalyzer;
 
-import com.zoolatech.loganalyzer.config.BeansConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 
 @SpringBootApplication
 public class LoganalyzerApplication2 {
 
     public static void main(String[] args) throws IOException {
-            SpringApplication.run(LoganalyzerApplication2.class, args);
-        }
+        SpringApplication.run(LoganalyzerApplication2.class, args);
     }
+}
 
