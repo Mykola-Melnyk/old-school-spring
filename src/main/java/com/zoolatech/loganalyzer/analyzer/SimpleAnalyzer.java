7a8edf6884af102e7d9c0a59ea3d8a20f7ebd140
Mykola Melnyk <mr.nicholas.x@gmail.com>
2022-10-30 10:13:05 +0200

    added tests for StubReader

--- src/main/java/com/zoolatech/loganalyzer/analyzer/SimpleAnalyzer.java
+++ src/main/java/com/zoolatech/loganalyzer/analyzer/SimpleAnalyzer.java
@@ -7,10 +7,6 @@ public class SimpleAnalyzer implements Analyzer {
     @Value("${find.me}")
     private String findMe;
 
-//    public SimpleAnalyzer(String findMe) {
-//        this.findMe = findMe;
-//    }
-
     @Override
     public boolean isMatch(String line) {
         return line.contains(findMe);
