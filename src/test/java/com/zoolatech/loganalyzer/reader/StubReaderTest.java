e37ba40eec3335a913485dc2a479ccfd9a97ec3b
Mykola Melnyk <mr.nicholas.x@gmail.com>
2022-10-30 09:28:10 +0200

    added test folder

---
+++ src/test/java/com/zoolatech/loganalyzer/reader/StubReaderTest.java
@@ -0,0 +1,13 @@
+package com.zoolatech.loganalyzer.reader;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StubReaderTest {
+
+    @Test
+    void nextLine() {
+    }
+
+}
